a67a7211080b1aba259aef76d3e8296c8a78d387
sevdaolgunn <sevdaolgun111@hotmail.com>
2022-09-10 10:50:58 +0300

    click event modified

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="backdrop" @click="closeModal">
+    <div class="backdrop" @click.self="closeModal">
         <div class="modal" :class="{sale: theme === 'sale'}">
             <h1>{{ header }}</h1>
             <p>{{text}}</p>
@@ -7,12 +7,12 @@
     </div>
 </template>
 <script>
-import { METHODS } from 'http';
+
 
     export default {
         props: ['header', 'text', 'theme'],
-        methods:{
-            closeModal(){
+        methods: {
+            closeModal() {
                 this.$emit('close')
 
             }
